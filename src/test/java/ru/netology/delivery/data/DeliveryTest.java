4b95e1db746424e7a1665e3fab96290a088f1d7d
Maria Minor <meverik88tt@gmail.com>
2022-09-27 15:38:13 +0300

    unit12

--- src/test/java/ru/netology/delivery/data/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/data/DeliveryTest.java
@@ -21,7 +21,7 @@ class DeliveryTest {
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
-    void shouldSuccessfulPlanAndReplanMeeting() {
+    void shouldSuccessfulPlanAndRePlanMeeting() {
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
